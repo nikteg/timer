03cbd051b0991f027ae58e771b8152b65a861ab6
nikteg <nikteg@users.noreply.github.com>
2020-04-04 12:43:43 +0000

    deploy: 776ccfd10708b789dbbf5d9942b4d32ae6d8d7a2

---
+++ precache-manifest.3c7a5603471694a2b89bc1b6f32f4303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b4b873187161ef436a0e914a2f7bf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88be5ed0c432aa7f2bae",
+    "url": "/static/css/main.12fd0d55.chunk.css"
+  },
+  {
+    "revision": "d8c72f249fda48372294",
+    "url": "/static/js/2.672c36e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.672c36e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88be5ed0c432aa7f2bae",
+    "url": "/static/js/main.60d0c324.chunk.js"
+  },
+  {
+    "revision": "08f1d0745b86d71c4472",
+    "url": "/static/js/runtime-main.3901ef34.js"
+  }
+]);
---
+++ service-worker-custom.js
@@ -0,0 +1,15 @@
+console.log("Hello from service worker");
+
+importScripts("./service-worker.js");
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: "window" }).then((clientList) => {
+      for (let client of clientList) {
+        if (client.url === "/" && "focus" in client) return client.focus();
+      }
+      if (clients.openWindow) return clients.openWindow("/");
+    })
+  );
+});
