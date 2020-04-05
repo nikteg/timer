923b3a8fe0134f5352d15f2c9fea15d0ca794fe9
Niklas <me@sodapop.se>
2020-04-05 10:33:59 +0200

    Update screenshots

--- capture.js
+++ capture.js
@@ -1,14 +1,15 @@
 const puppeteer = require("puppeteer");
 
 const captureScreenshot = async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.setViewport({ width: 360, height: 640 });
+  await page.setViewport({ width: 576, height: 1024 });
   await page.goto("http://localhost:3000");
   await page.screenshot({ path: "./docs/screenshot.png", fullPage: true });
 
   await page.goto("http://localhost:3000/timer?duration=3600&remaining=400");
+  await page.waitForSelector("#skip");
   await page.click("#skip");
   await page.screenshot({ path: "./docs/screenshot2.png", fullPage: true });
 
